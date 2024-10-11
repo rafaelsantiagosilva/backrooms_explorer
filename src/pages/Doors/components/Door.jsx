71c6f1a0c35e223656effd5d64af25e19b7ddd08
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-10-10 21:10:58 -0300

    Substituindo a tag de âncora pelo componente Link do react-router-dom

--- src/pages/Doors/components/Door.jsx
+++ src/pages/Doors/components/Door.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { GiCardJoker, GiMonsterGrasp } from "react-icons/gi";
 import { forwardRef, useState } from "react";
 
@@ -57,13 +57,13 @@ const Door = forwardRef((props, ref) => {
 
   return (
     <div onMouseEnter={() => { playDoorSound(); setIsAnimation('door-animation.gif') }} onMouseLeave={() => setIsAnimation('door.png')}>
-      <a onClick={generateRandomNextRoom} ref={ref} href={`/rooms${typeNextRoom}/${Number(number) + 1}`} draggable="false" className="group" data-prob={probability} data-icon={icon}>
+      <Link onMouseDown={generateRandomNextRoom} ref={ref} to={`/rooms${typeNextRoom}/${Number(number) + 1}`} draggable="false" className="group" data-prob={probability} data-icon={icon}>
         <img className="w-80" src={`/assets/${isAnimation}`} alt="Porta de madeira" draggable="false" />
         <div className="relative w-12 h-12 bottom-2/3 left-[8.8rem] flex flex-col justify-center items-center text-zinc-500 text-center group-hover:text-zinc-400">
           <span className="text-6xl">{icon}</span>
           <span className="text-lg invisible group-hover:visible">{probability}%</span>
         </div>
-      </a>
+      </Link>
     </div>
   );
 });
